6aa20295e71e7f9614093222b92af244cc9a3905
Yang Feng <fyfindqq@163.com>
2016-04-28 16:29:36 -0400

    stack buf

--- final/mysort.cpp
+++ final/mysort.cpp
@@ -12,6 +12,7 @@
 #include <algorithm>
 using namespace std;
 
+#define BUF 1000
 int numtasks, rank;
 
 #define handle_error(msg) \
@@ -26,12 +27,18 @@ struct mykey {
 };
 
 struct myrow {
-    char v[100];
+    char buf[BUF + BUF][100];
+    //char *buf[BUF + BUF];
+    int p;
     int ix;
-    bool operator <(const myrow &b) const {
-        return strcmp(v, b.v) > 0;
-    }
+    //bool operator <(const myrow &b) const {
+    //    return strcmp(buf[p], b.buf[p]) > 0;
+    //}
 };
+
+bool lt(const myrow *a, const myrow *b) {
+    return strcmp(a->buf[a->p], b->buf[b->p]) > 0;
+}
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
@@ -71,11 +78,23 @@ int main(int argc, char *argv[]) {
         }
 
         sort(key, key + len);
+
+        int td = open("/localdisk/yfeng23/tmp", O_RDWR | O_CREAT | O_TRUNC, S_IRWXU);
+        lseek(td, sb.st_size - 1, SEEK_SET);
+        write(td, "", 1);
+        char *taddr;
+        taddr = (char*)mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, td, 0);
+
         for (i = 0; i < len; i++) {
-            MPI_Send(addr + key[i].ix * 100, 100, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+            memcpy(taddr + i * 100, addr + key[i].ix * 100, 100);
         }
         munmap(addr, sb.st_size);
         close(fd);
+        for (i = 0; i < len; i+=BUF) {
+            MPI_Send(taddr + i * 100, 100 * BUF, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        }
+        munmap(taddr, sb.st_size);
+        close(td);
     }
     else {
         struct timeval start;
@@ -84,31 +103,56 @@ int main(int argc, char *argv[]) {
         sizebuf = new long long[numtasks];
         MPI_Gather(&len, 1, MPI_LONG_LONG, sizebuf, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
         long long total = 0;
-        myrow buf[numtasks];
+        myrow row[numtasks];
+        myrow *prow[numtasks];
         MPI_Status stat;
+        MPI_Request reqs[numtasks];
         for (int j = 1; j < numtasks; j++) {
             total += sizebuf[j];
-            MPI_Recv(buf[j].v, 100, MPI_CHAR, j, 0, MPI_COMM_WORLD, &stat);
-            buf[j].ix = j;
-            sizebuf[j]--;
+            //for (int k = 0; k < BUF + BUF; k++) {
+            //    row[j].buf[k] = new char[100];
+            //}
+            MPI_Recv(row[j].buf, 100 * BUF, MPI_CHAR, j, 0, MPI_COMM_WORLD, &stat);
+            //MPI_Irecv(row[j].buf, 100 * BUF, MPI_CHAR, j, 0, MPI_COMM_WORLD, &reqs[j]);
+            //MPI_Wait(&reqs[j], &stat);
+            MPI_Irecv(row[j].buf + BUF, 100 * BUF, MPI_CHAR, j, 0, MPI_COMM_WORLD, &reqs[j]);
+            //MPI_Wait(&reqs[j], &stat);
+            row[j].ix = j;
+            row[j].p = 0;
+            sizebuf[j] -= BUF;
+            prow[j] = row + j;
         }
         cout << "total " << total << endl;
         fd = open("/localdisk/yfeng23/res", O_RDWR | O_CREAT | O_TRUNC, S_IRWXU);
         lseek(fd, total * 100 - 1, SEEK_SET);
         write(fd, "", 1);
         addr = (char*)mmap(NULL, total * 100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-        make_heap(buf + 1, buf + numtasks);
+        make_heap(prow + 1, prow + numtasks, lt);
         for (i = 0; i < total; i++) {
-            //printf("%d %d\t%d\n",sizebuf[1], sizebuf[2], buf[1].ix);
-            memcpy(addr + i * 100, buf[1].v, 100);
-            pop_heap(buf + 1, buf + numtasks);
-            if (sizebuf[buf[numtasks - 1].ix]) {
-                MPI_Recv(buf[numtasks - 1].v, 100, MPI_CHAR, buf[numtasks - 1].ix, 0, MPI_COMM_WORLD, &stat);
-                sizebuf[buf[numtasks - 1].ix]--;
-                push_heap(buf + 1, buf + numtasks);
-            }
-            else {
-                numtasks--;
+            //printf("%d %d\t%d\n",sizebuf[1], sizebuf[2], prow[1]->ix);
+            memcpy(addr + i * 100, prow[1]->buf[prow[1]->p], 100);
+            pop_heap(prow + 1, prow + numtasks, lt);
+            prow[numtasks - 1]->p++;
+            if (prow[numtasks - 1]->p == BUF + BUF)
+                prow[numtasks - 1]->p = 0;
+            int j = prow[numtasks - 1]->ix;
+            if (prow[numtasks - 1]->p % BUF == 0) {
+                if (sizebuf[j]) {
+                    //puts("start wait");
+                    MPI_Wait(&reqs[j], &stat);
+                    //puts("end wait");
+                    if (prow[numtasks - 1]->p == BUF) {
+                        MPI_Irecv(prow[numtasks - 1]->buf, 100 * BUF, MPI_CHAR, j, 0, MPI_COMM_WORLD, &reqs[j]);
+                    }
+                    else {
+                        MPI_Irecv(prow[numtasks - 1]->buf + BUF, 100 * BUF, MPI_CHAR, j, 0, MPI_COMM_WORLD, &reqs[j]);
+                    }
+                    sizebuf[j] -= BUF;
+                    push_heap(prow + 1, prow + numtasks, lt);
+                }
+                else {
+                    numtasks--;
+                }
             }
             if (i % (total/10) == 0) {
                 puts("10\% finished");
